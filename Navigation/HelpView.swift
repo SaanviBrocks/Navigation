ec5793f54914ab04c9cec1fdfaaf9468d275ef08
Saanvi <sbhosekar01@gmail.com>
2025-07-24 12:45:44 -0500

    added help view

---
+++ Navigation/HelpView.swift
@@ -0,0 +1,18 @@
+//
+//  HelpView.swift
+//  Navigation
+//
+//  Created by Sunil Bhosekar on 7/24/25.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text("Help view")
+    }
+}
+
+#Preview {
+    HelpView()
+}
